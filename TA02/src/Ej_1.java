6e460e4e5cdfa76b36a454641df0cb02a119c063
EricMuntas <112870448+EricMuntas@users.noreply.github.com>
2023-04-20 14:39:32 +0200

    Update Ej_1.java
    Descomentada la línea 5

--- TA02/src/Ej_1.java
+++ TA02/src/Ej_1.java
@@ -2,7 +2,7 @@
 public class Ej_1 {
 
 	public static void main(String[] args) {
-		//System.out.println("Hola");
+		System.out.println("Hola");
 		System.out.println("Adios");
 		System.out.println("FIN DE PROGRAMA");
 	}
